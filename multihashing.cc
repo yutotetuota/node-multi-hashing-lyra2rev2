13e19f133e94723802707da4f49ee09b9abef51b
saymissme <saymissme@qq.com>
2015-05-03 15:13:45 +0800

    add lyra2re

--- multihashing.cc
+++ multihashing.cc
@@ -24,6 +24,7 @@ extern "C" {
     #include "x15.h"
     #include "whirlpoolx.h"
     #include "fresh.h"
+    #include "Lyra2RE.h"
 }
 
 #include "boolberry.h"
@@ -597,6 +598,27 @@ Handle<Value> whirlpoolx(const Arguments& args) {
     return scope.Close(buff->handle_);
 }
 
+Handle<Value> lyra2re(const Arguments& args) {
+    HandleScope scope;
+
+    if (args.Length() < 1)
+        return except("You must provide one argument.");
+
+    Local<Object> target = args[0]->ToObject();
+
+    if(!Buffer::HasInstance(target))
+        return except("Argument should be a buffer object.");
+
+    char * input = Buffer::Data(target);
+    char output[32];
+
+    uint32_t input_len = Buffer::Length(target);
+
+    lyra2re_hash(input, output, input_len);
+
+    Buffer* buff = Buffer::New(output, 32);
+    return scope.Close(buff->handle_);
+}
 void init(Handle<Object> exports) {
     exports->Set(String::NewSymbol("quark"), FunctionTemplate::New(quark)->GetFunction());
     exports->Set(String::NewSymbol("x11"), FunctionTemplate::New(x11)->GetFunction());
@@ -621,6 +643,7 @@ void init(Handle<Object> exports) {
     exports->Set(String::NewSymbol("x15"), FunctionTemplate::New(x15)->GetFunction());
     exports->Set(String::NewSymbol("whirlpoolx"), FunctionTemplate::New(whirlpoolx)->GetFunction());
     exports->Set(String::NewSymbol("fresh"), FunctionTemplate::New(fresh)->GetFunction());
+    exports->Set(String::NewSymbol("lyra2re"), FunctionTemplate::New(lyra2re)->GetFunction());
 }
 
 NODE_MODULE(multihashing, init)
