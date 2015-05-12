3fe2e12b51229dfc9c8bb7e8ca7e90eaaa6e504e
saymissme <saymissme@qq.com>
2015-05-13 01:09:34 +0800

    fix error

--- Lyra2RE.c
+++ Lyra2RE.c
@@ -32,10 +32,10 @@
 #include <stdint.h>
 #include <string.h>
 #include <stdio.h>
-#include "sph_blake.h"
-#include "sph_groestl.h"
-#include "sph_keccak.h"
-#include "sph_skein.h"
+#include "sha3/sph_blake.h"
+#include "sha3/sph_groestl.h"
+#include "sha3/sph_keccak.h"
+#include "sha3/sph_skein.h"
 #include "Lyra2.h"
 
 void lyra2re_hash(const char* input, char* output)
